fe183f3c1e869881a740d141c0f3bdb70cbec9e3
Michael Hueschen <mhueschen@galois.com>
2016-05-04 19:42:42 -0700

    Fix up C2 for bytes

--- michael/C2.hs
+++ michael/C2.hs
@@ -11,8 +11,5 @@ isCorrect = (myXOR "1c0111001f010100061a024b53535009181c"
             == "746865206b696420646f6e277420706c6179"
 
 myXOR :: [Char] -> [Char] -> [Char]
-myXOR (a:b:c:rest1) (d:e:f:rest2) = x:y:z:(myXOR rest1 rest2)
-  where
-  (x,y,z) = encodeHex (decodeHex (a,b,c) `xor` decodeHex (d,e,f))
-myXOR [] [] = []
-myXOR _  _  = error "Inputs did not have multiple-of-3 lengths"
+myXOR xs ys = encodeHex (zipWith (xor) (decodeHex xs)
+                                       (decodeHex ys))
