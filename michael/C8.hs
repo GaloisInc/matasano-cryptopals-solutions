2e3d943f05983948c45cc07ad582970f3b265e17
Michael Hueschen <mhueschen@galois.com>
2016-05-10 16:26:30 -0700

    #8 booyah

---
+++ michael/C8.hs
@@ -0,0 +1,29 @@
+module C8 where
+
+import Control.Monad
+import Data.List.Split (chunksOf)
+import qualified Data.Set as S
+
+import C1 hiding ( isCorrect )
+
+checkRepeat :: Ord a => [a] -> (Bool,[[a]],[[a]])
+checkRepeat bs = ( origLen /= dedupLen
+                 , chunks
+                 , deduped )
+  where
+  chunks = chunksOf 16 bs
+  origLen = length chunks
+  deduped = (S.toList (S.fromList chunks))
+  dedupLen = length deduped
+
+main :: IO ()
+main = do
+  cipherBytesList <- (map decodeHex . lines) <$> readFile "8.txt"
+  forM_ cipherBytesList $ \bs -> do
+    let (doesRepeat, orig, deduped) = checkRepeat bs
+    if doesRepeat
+       then do putStrLn "Repeat detected!"
+               putStrLn $ "Orig: " ++ show orig
+               putStrLn $ "Deduped: " ++ show deduped
+       else return ()
+
